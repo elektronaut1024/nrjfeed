8cffdcefa2692dcbf1079936add9af22f2ef9ff6
Lars Schultz <lars.schultz@toolpark.com>
2014-01-16 23:41:42 +0100

    FIX: server timeout issues

--- transceiver/transceiver.ino
+++ transceiver/transceiver.ino
@@ -3,89 +3,197 @@
 
 byte mac[] = {0xDE, 0xCD, 0xAE, 0x0F, 0xFE, 0xED };
 
+boolean HTTP = true;
 EthernetClient client;
 IPAddress forwardHostIP(80,74,143,90);
+int forwardingDelay = 2000;
 
 int serialBufferLength = 0;
 int serialBufferLimit = 1000;
 char serialBuffer[1001] = "";
 
 boolean startedSerial = false;
-  
-void setup() {
-  if (Ethernet.begin(mac) == 0) {
-    for(;;) ; //failed to retrieve IP from DHCP
-  }
-  
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  
-  if ( Serial ) Serial.println("started");
-}
 
-void readSerialData() {
-  if ( !Serial1 ) return;
-  if ( Serial1.available() <= 0 ) return;
- 
-  if ( Serial ) {
-    Serial.print(Serial1.available());
-    Serial.println(" data available.");
-  }
-  
-  while ( Serial1.available() > 0 && serialBufferLength < serialBufferLimit ) {
-    serialBuffer[serialBufferLength++] = Serial1.read();
-    serialBuffer[serialBufferLength] = 0;
-  }
-  
-  if ( Serial ) {
-    Serial.print("Content-Length: ");
-    Serial.println(serialBufferLength);
-    Serial.print("data=");
-    Serial.println(serialBuffer);
-    Serial.println();
-  }
-  
-  if ( forwardSerialData() ) {
-    serialBufferLength = 0;
-    if ( Serial ) {
-      Serial.println();
-      Serial.println("finished successfully");
-    }
-  } else {
-     if ( Serial ) {
-       Serial.println("failed to transmit data");
-     }
-  }
-}
+unsigned int waitInc = 1;
 
-boolean forwardSerialData() {
-  if ( !client.connect(forwardHostIP, 80) ) {
-    return false;
-  }
+boolean isStarted = false;
 
-  client.println("POST /nrjfeed/ HTTP/1.1");
-  client.println("Host: www.elektronaut.ch");
-  client.println("Connection: close");
-  client.println("Content-Type: application/x-www-form-urlencoded");
-  client.print("Content-Length: ");
-  client.println(serialBufferLength+5);
-  client.println();
-  client.print("feed=");
-  client.println(serialBuffer);
-  
-  while ( client.connected() ) {
-    if ( client.available() ) {
-      char c = client.read();
-      if ( Serial ) Serial.write(c);
-    }
-  }
+boolean stopHTTP(){
+  if ( !isStarted ) return false;
   
   client.stop();
+  isStarted = false;
   
   return true;
 }
 
+boolean connectHTTP(){
+  if ( client.connected() ) return true;
+  stopHTTP();
+  
+  if ( !client.connect(forwardHostIP, 80) ) {
+    if ( Serial ) {
+      Serial.print("failed to connect. waiting ");
+      Serial.print(waitInc);
+      Serial.println("ms and retry again");
+    }
+    delay(waitInc);
+    if ( waitInc < 32000 ) waitInc = waitInc * 2;
+    
+    return false; //discard any data because it is lost anyway
+  }
+  
+  isStarted = true;
+  
+  if ( Serial ) Serial.println("Connected");
+  
+  waitInc = 1;
+  
+  return true;
+}
+
+char packageEnd[] = "]]\r\n";
+int packageEndPos = 0;
+
+void readSerialData() {
+  if ( !Serial1 ) return;
+  if ( Serial1.available() <= 0 ) return;
+  
+  boolean forward = false;
+   
+  while ( Serial1.available() > 0 ) {
+    if ( serialBufferLength >= serialBufferLimit ) {
+      forward = true;
+      Serial.println("buffer full");
+      break;
+    }
+    char c = Serial1.read();
+    serialBuffer[serialBufferLength++] = c;
+    serialBuffer[serialBufferLength] = 0;
+    
+    if ( packageEnd[packageEndPos] == c ) {
+      packageEndPos++;
+      if ( packageEnd[packageEndPos] == 0 ) {
+        forward = true;
+        break;
+      }
+    } else {
+      packageEndPos = 0;
+    }
+  }
+  
+  if ( !forward ) return; //nothing worth forwarding
+  if ( !forwardData(serialBuffer,serialBufferLength) ) return; //not forwarded for some reason
+
+  serialBufferLength = 0;
+}
+
+boolean forwarding = false;
+unsigned long lastForward = 0;
+
+boolean forwardData(const char* data, const int length) {
+  if ( forwarding ) return false; //already forwarding something
+  
+  unsigned long now = millis();
+  if ( now - lastForward < forwardingDelay ) {
+    if ( HTTP && !client.connected() && stopHTTP() ) {
+      if ( Serial ) Serial.println("Server disconnected");
+    }
+    return false; //skip update
+  }
+  
+  if ( Serial ) Serial.print(data);
+  
+  if ( !HTTP ) return true;
+  if ( !connectHTTP() ) return false;
+  
+  forwarding = true;
+    
+  client.println("POST /nrjfeed/ HTTP/1.1");
+  client.println("Host: www.elektronaut.ch");
+  client.println("Content-Type: application/x-www-form-urlencoded");
+  client.print("Content-Length: ");
+  client.println(length+5);
+  client.println();
+  client.print("feed=");
+  client.println(data);
+  
+  lastForward = now;
+  
+  return true;
+}
+
+int finishHeaderPos = 0;
+char finishHeader[] = "\r\n";
+boolean finishedHeader = false;
+
+int connectionCloseHeaderPos = 0;
+char connectionCloseHeader[] = "\r\nConnection: close";
+
+void finishForwarding(){
+  if ( !forwarding ) return;
+  
+  if ( !client.available() ) return;
+  if ( !finishedForwarding() ) return;
+  
+  forwarding = false;
+}
+
+boolean finishedForwarding() {
+  while ( client.connected() && client.available() ) {
+    char c = client.read();
+    
+    if ( connectionCloseHeader[connectionCloseHeaderPos] == c ) {
+      connectionCloseHeaderPos++;
+      if ( connectionCloseHeader[connectionCloseHeaderPos] == 0 ) {
+        if ( Serial ) Serial.println("Server will close connection");
+        return true;
+      }
+    } else {
+      connectionCloseHeaderPos = 0;
+    }
+ 
+    if ( finishHeader[finishHeaderPos] == c ) {
+      finishHeaderPos++;
+      if ( finishHeader[finishHeaderPos] == 0 ) {
+        if ( finishedHeader ) { //if a header was finished before, the whole header section is now terminated
+          if ( Serial ) Serial.println("Continue");
+          return true;
+        } else {
+          finishHeaderPos = 0;
+          finishedHeader = true;
+        }
+      }
+    } else {
+      finishHeaderPos = 0;
+      finishedHeader = false;
+    }
+  }
+  
+  if ( Serial ) Serial.println("Not finished");
+  
+  return false;
+}
+
+void setup() {
+  Serial1.begin(9600);
+  
+  Serial.begin(9600);
+  
+  if ( HTTP ) {
+    delay(1000);
+    if ( Serial ) Serial.println("Trying for Ethernet");
+    
+    if (Ethernet.begin(mac) == 0) {
+      if ( Serial ) Serial.println("No Ethernet");
+      HTTP = false;
+    }
+  }
+  
+  if ( Serial ) Serial.println("started");
+}
+
 void loop() {
   readSerialData();
-  delay(1000);
+  finishForwarding();
 }
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,7 +1,4 @@
 int inputPin = 0;
-int impulsePin = 7;
-int errorPin = 8;
-int powerPin = 6;
 
 //Sensor State
 int lightValue = 0;
@@ -14,7 +11,6 @@ boolean initialized = false;
 unsigned long now = 0;
 
 //Transmission
-int transmissionIndicator = 0;
 unsigned long transmissionIdx = 0;
 
 //Transmission interval
@@ -26,31 +22,59 @@ int dataLength = 0;
 int maxDataLength = 5;
 unsigned long data[5];
 
+void setup() {
+  Serial.begin(9600);
+}
+
+unsigned long lastImpulse = 0;
+
+void loop() {
+  now = millis();
+ 
+ if ( now - lastImpulse > 100 ) {
+   lightValue = lightThreshold + 1;
+   lastImpulse = now;
+ } else {
+   lightValue = 0;
+ }
+ 
+ //lightValue = analogRead(inputPin);
+  
+  if ( lightValue > lightThreshold ) impulseOn();
+  else impulseOff();
+  
+  rollover();
+  
+  delay(10);
+}
+
+/**
+ * A package should consist of less than 56 bytes,
+ * which should always fit into the transmitter buffer
+ **/
 void transmit(){
   transmissionIdx++;
   
+  Serial.flush(); //if the previous package was not fully sent, this waits for it to be sent out.
+  
   Serial.print("[");
-  Serial.print(transmissionIdx);
+  Serial.print(transmissionIdx); //max 10 chars (4bytes)
   Serial.print(",");
-  Serial.print(lastTransmission);
+  Serial.print(lastTransmission); //max 10 chars (4bytes)
   Serial.print(",[");
   
   for ( int i=0; i<dataLength; i++ ) {
     if ( i > 0 ) Serial.print(",");
     
-    Serial.print(data[i]-lastTransmission);
+    Serial.print(data[i]-lastTransmission);//max 5 chars
   }
   
   Serial.println("]]");
 }
 
 void impulseOn(){
-  if ( isBright == true ) {  
-    return;
-  }
+  if ( isBright == true ) return;
   isBright = true;
-  
-  digitalWrite(impulsePin,HIGH);
   
   data[dataLength] = now;
   dataLength++;
@@ -59,8 +83,6 @@ void impulseOn(){
 void impulseOff(){
   if ( isBright == false ) return;
   isBright = false;
-  
-  digitalWrite(impulsePin,LOW);
 }
 
 void rollover() {
@@ -75,91 +97,4 @@ void rollover() {
      lastTransmission = now;
      dataLength=0;
    }
-}
-
-void blinkTransmissionIndicator(){
-  if ( transmissionIndicator <= 0 ) return;
-  
-  transmissionIndicator--;
-  
-  if ( transmissionIndicator <= 0 ) {
-    digitalWrite(errorPin,LOW);
-  } else {
-    if ( transmissionIndicator % 10 > 5 ) digitalWrite(errorPin,LOW);
-    else digitalWrite(errorPin,HIGH);
-  }
-}
-
-void measure() {
-  now = millis();
-  
-  lightValue = analogRead(inputPin);
-  
-  if ( lightValue > lightThreshold ) impulseOn();
-  else impulseOff();
-  
-  blinkTransmissionIndicator();
-  
-  rollover();
-  
-  delay(10);
-}
-/*
-int minLightValue = -1;
-int minLightValueHigh = -1;
-
-int maxLightValue = 0;
-int maxLightValueLow = 0;
-
-int middleLightValue = 0;
-
-void tuneIn() {
-  lightValue = analogRead(inputPin);
-  
-  if ( minLightValue < 0 ) {
-    minLightValue = lightValue;
-    maxLightValue = lightValue;
-    minLightValueHigh = lightValue;
-    maxLightValueLow = lightValue;
-  } else {
-    minLightValue = min(minLightValue,lightValue);
-    maxLightValue = max(maxLightValue,lightValue);
-    
-    int newMiddleLightValue = round((maxLightValue - minLightValue) / 2);
-    if ( newMiddleLightValue != middleLightValue ) {
-      middleLightValue = newMiddleLightValue;
-      minLightValueHigh = 0;
-      maxLightValueLow = lightValue;
-    } else {
-      if ( lightValue < middleLightValue) minLightValueHigh = max(minLightValueHigh,lightValue);
-      else maxLightValueLow = min(maxLightValueLow,lightValue);
-    }
-  }
-  
-  if( millis() > 3000 ) {
-    initialized = true;
-    //lightThreshold = middleLightValue;
-  }
-  
-  delay(10);
-}
-*/
-
-void setup() {
-  pinMode(errorPin,OUTPUT);
-  pinMode(impulsePin,OUTPUT);
-  pinMode(powerPin,OUTPUT);
-  
-  digitalWrite(errorPin,LOW);
-  digitalWrite(impulsePin,LOW);
-  digitalWrite(powerPin,HIGH);
-  
-  Serial.begin(9600);
-}
-
-void loop() {
-  measure();
-  /*if ( initialized ) measure();
-  else tuneIn();
-  */
 }
